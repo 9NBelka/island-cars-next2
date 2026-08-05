44527c8bd1cccc71c3d312039f821d4eb5dcca14
Nikita <belka.nik02@gmail.com>
2026-08-05 11:07:26 +0200

    delete test text

--- app/components/Contacts/Contacts.tsx
+++ app/components/Contacts/Contacts.tsx
@@ -28,7 +28,7 @@ export default function Contacts({ lang }: ContactsProps) {
             <p className={styles.label}>{t('contacts.label')}</p>
 
             <h1 className={styles.title}>
-              {t('contacts.title')} ававав
+              {t('contacts.title')}{' '}
               <span className={styles.accent}>{t('contacts.titleAccent')}</span>
             </h1>
 
